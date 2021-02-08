2e8d18376f3fae6aaf27fce9da3c26bc07f77083
fttang14 <puddingmilktea@outlook.com>
2021-02-08 15:45:11 -0800

    production build v0.1

--- server/src/server.js
+++ server/src/server.js
@@ -50,9 +50,9 @@ app.use("/register", registerRouter);
 
 // Build production init
 if(process.env.NODE_ENV === "production"){
-	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.use(express.static(path.join(__dirname, "../../client/build")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "../client/build/index.html"));
+		res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 	});
 }
 
